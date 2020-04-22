7d9ab819b9a014246529da69e8e5c0533f802766
Eetu Kivelä <eetu.kivelae@gmail.com>
2020-04-22 13:59:27 +0300

    Made basic working version of bogosort

--- Program.fs
+++ Program.fs
@@ -1,11 +1,37 @@
 open System
 
+let rand = new System.Random()
+
+let mutable iters : int = 0
+
+let swap (arr: _[]) x y =
+    let tmp = arr.[x]
+    arr.[x] <- arr.[y]
+    arr.[y] <- tmp
+
+let shuffle sArray =
+    Array.iteri (fun i _ -> swap sArray i (rand.Next(i, Array.length sArray))) sArray
+
+
+let rec checkArray sorted sortResult =
+    let sortStatus : bool = sorted = sortResult
+    if sortStatus then
+        sortResult
+    else
+        printfn"%A" sortResult
+        iters <- iters + 1
+        shuffle sortResult
+        checkArray sorted sortResult
+
+
 [<EntryPoint>]
 let main argv =
+    let arrayLength : int = 9
+    let sortedArray : int array = [|for i in 1 .. arrayLength -> i |]
+    let mutable arrayToSort : int array = Array.copy sortedArray
+    shuffle arrayToSort
 
-  let mutable number = 1
-
-  for i in 1..10 do
-    number <- number + number
-    printfn "%d" number
-  0 // return an integer exit code
+    printfn"%A" (checkArray sortedArray arrayToSort)
+    printf"Iterations: "
+    printfn"%i" iters
+    0 // return an integer exit code
